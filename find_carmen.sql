88fe2a6ec222a0d769173e8bf92f66b822616e39
Oleh <kravetsoleh4991@gmail.com>
2023-09-14 22:56:08 -0700

    commit2

--- find_carmen.sql
+++ find_carmen.sql
@@ -3,7 +3,7 @@
 -- use two `-` to comment at the start of a line, or highlight the text and press `⌘/` to toggle comments
 -- EXAMPLE: SELECT ALL FROM THE TABLE COUNTRY AND LIMIT IT TO ONE ENTRY
 
-SELECT * FROM COUNTRY LIMIT 1;
+-- SELECT * FROM COUNTRY LIMIT 1;
 
 --  -[ RECORD 1 ]--+--------------------------
 -- code           | AFG
@@ -25,31 +25,70 @@ SELECT * FROM COUNTRY LIMIT 1;
 
 -- Clue #1: We recently got word that someone fitting Carmen Sandiego's description has been traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed, so find the least populated country in Southern Europe, and we'll start looking for her there.
 
+    -- Southern Europe
+    -- country least populated
+    SELECT 
+        name,
+        population
+    FROM country
+    WHERE population < 2000
+    AND region = 'Southern Europe';
+
+    -- Carmen is in Holy See (Vatican City State)
 
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in this country's officially recognized language. Check our databases and find out what language is spoken in this country, so we can call in a translator to work with you.
 
+    -- Language join
+    SELECT * FROM countrylanguage
+    LEFT OUTER JOIN country 
+    ON country.code = countrylanguage.countrycode
+    WHERE name='Holy See (Vatican City State)';
 
-
+    -- Language = Italian
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on to a different country, a country where people speak only the language she was learning. Find out which nearby country speaks nothing but that language.
 
-
-
+    -- Language join
+    SELECT * FROM countrylanguage
+    LEFT OUTER JOIN country 
+    ON country.code = countrylanguage.countrycode
+    WHERE language='Italian' AND percentage='100'
+    
+    -- Country is San Marino
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time. There are only two cities she could be flying to in the country. One is named the same as the country – that would be too obvious. We're following our gut on this one; find out what other city in that country she might be flying to.
 
+    -- join city to country table
+    SELECT * FROM city 
+    JOIN country
+    ON country.code = city.countrycode
+    WHERE country.name='San Marino' AND city.name != country.name;
 
+    -- City = Serravalle
 
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
+    -- join city to country table
+    SELECT * FROM city 
+    JOIN country
+    ON country.code = city.countrycode
+    WHERE city.name ILIKE 'serra';
 
+    -- city = Serra
+    -- country = Brazil
 
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll follow right behind you!
 
+    -- join city to country table
+    SELECT city.name FROM city 
+    JOIN country
+    ON city.id = country.capital
+    WHERE country.name = 'Brazil';
 
+    --city = Braslia
 
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the landing dock.
@@ -66,7 +105,11 @@ SELECT * FROM COUNTRY LIMIT 1;
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
 
+-- Find city population with either 91,084/85
+    SELECT city.name FROM city 
+    JOIN country
+    ON city.countrycode = country.code
+    WHERE city.population = 91084;
 
 
-
--- She's in ____________________________!
+-- She's in Santa Monica!
--- nfl.sql
+++ nfl.sql
@@ -1,40 +1,54 @@
 -- 1. List the names of all NFL teams'
-
+SELECT * FROM teams
 
 -- 2. List the stadium name and head coach of all NFC teams
-
+SELECT teams.stadium, teams.head_coach FROM teams WHERE teams.conference = 'NFC'
 
 -- 3. List the head coaches of the AFC South
-
+SELECT teams.head_coach FROM teams WHERE teams.conference = 'AFC' AND teams.division = 'South'
 
 -- 4. The total number of players in the NFL
-
+ SELECT COUNT(players.name) FROM players
 
 -- 5. The team names and head coaches of the NFC North and AFC East
-
+SELECT teams.name, teams.head_coach FROM teams 
+WHERE teams.conference = 'NFC' AND teams.division = 'North'
+OR teams.conference = 'AFC' AND teams.division = 'East'
 
 -- 6. The 50 players with the highest salaries
-
+SELECT * FROM players ORDER BY players.salary DESC LIMIT 50
 
 -- 7. The average salary of all NFL players
-
+SELECT AVG(players.salary) FROM players
 
 -- 8. The names and positions of players with a salary above 10_000_000
-
+SELECT players.name, players.position FROM players WHERE players.salary > 10000000
 
 -- 9. The player with the highest salary in the NFL
-
+SELECT * FROM players ORDER BY players.salary DESC LIMIT 1
 
 -- 10. The name and position of the first 100 players with the lowest salaries
-
+SELECT players.name, players.position FROM players ORDER BY players.salary ASC LIMIT 100
 
 -- 11. The average salary for a DE in the nfl
-
+SELECT AVG(players.salary) FROM players WHERE players.position = 'DE'
 
 -- 12. The names of all the players on the Buffalo Bills
-
+--USE JOIN
+SELECT * FROM players JOIN teams 
+ON players.team_id = teams.id
+WHERE teams.name = 'Buffalo Bills'
 
 -- 13. The total salary of all players on the New York Giants
-
+--USE JOIN
+SELECT SUM(players.salary) FROM players JOIN teams
+ON players.team_id = teams.id
+WHERE teams.name = 'New York Giants'
 
 -- 14. The player with the lowest salary on the Green Bay Packers
+--USE JOIN
+SELECT * FROM players JOIN teams
+ON players.team_id = teams.id
+WHERE teams.name = 'Green Bay Packers'
+ORDER BY players.salary ASC
+LIMIT 1
